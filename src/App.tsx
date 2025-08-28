c93402a3b0e1bdb97ddd884efbfc3d8f024df32d
blaablaa1234 <MD122436@greendreamteam.onmicrosoft.com>
2025-08-28 11:47:21 +0300

    fixed all

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React , { useState } from 'react'
-import './App.css'
-import MissionsCard from './components/Card'
-import { Grid } from '@mui/material'
-import { missionItems } from './components/To-Do-List'
-import { MissionData } from './components/To-Do-List'
+import React, { useState } from "react";
+import "./App.css";
+import MissionsCard from "./components/Card";
+import { Grid, Box } from "@mui/material";
+import { missionItems } from "./components/TodoList";
+import { MissionData } from "./components/TodoList";
 import { CiBoxList } from "react-icons/ci";
-function App() {
 
+function App() {
   return (
     <>
-    <h1>TODO-LIST <CiBoxList style={{marginBottom:"-7"}}/></h1>
-      <Grid container spacing={2} sx={{p:10}} >
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <h1 style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          TODO-LIST <CiBoxList size={66} />
+        </h1>
+      </Box>
+      <Grid container spacing={2} sx={{ p: 10 }}>
         {missionItems.map((missionData) => (
           <Grid key={missionData.id}>
-            <MissionsCard data={missionData}/>
+            <MissionsCard data={missionData} />
           </Grid>
         ))}
       </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,50 @@
 import { Card, CardContent, Typography } from "@mui/material";
-import { MissionData } from "./To-Do-List";
-import '../App.css'
-interface CardProps {
-    data: MissionData
-}
+import { MissionData } from "./TodoList";
+import { styled } from "@mui/material/styles";
 
-const MissionsCard: React.FC<CardProps> = ({data}) => {
-    return (
-        <Card sx={{width:400,
-            backgroundColor: 'lightblue',
-            padding: 2,
-            borderRadius: 2,
-            boxShadow: 3,
-            height:130,
-            display:"grid"
-        }} className="card">
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div" className="card-title" sx={{fontFamily:"Marker Felt, fantasy"}}>
-                     {data.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary" className="card-body" sx={{fontFamily:"Trebuchet MS, sans-serif	"}}>
-                    <b>Difficulty:</b> {data.difficulty}
-                    <Typography variant="body2" color="text.secondary" className="card-body" sx={{fontFamily:"Trebuchet MS, sans-serif	"}}>
-                    <b>Completed:</b>  {data.completed.toString()}
-                    </Typography>
-                </Typography>
-            </CardContent>
-        </Card>
-    )
+interface CardProps {
+  data: MissionData;
 }
-export default MissionsCard
+const StyledCard = styled(Card)(({ theme }) => ({
+  width: 400,
+  backgroundColor: "lightblue",
+  padding: theme.spacing(2),
+  borderRadius: 16,
+  boxShadow: theme.shadows[3],
+  height: 130,
+  display: "flex",
+}));
+const MissionsCard: React.FC<CardProps> = ({ data }) => {
+  return (
+    <StyledCard>
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          className="card-title"
+          sx={{ fontFamily: "Marker Felt, fantasy" }}
+        >
+          {data.title}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="card-body"
+          sx={{ fontFamily: "Trebuchet MS, sans-serif	" }}
+        >
+          <b>Difficulty:</b> {data.difficulty}
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            className="card-body"
+            sx={{ fontFamily: "Trebuchet MS, sans-serif	" }}
+          >
+            <b>Completed:</b> {data.completed.toString()}
+          </Typography>
+        </Typography>
+      </CardContent>
+    </StyledCard>
+  );
+};
+export default MissionsCard;
